69d9aee18c14f4a76dba5674389f5d338b1e578a
kapy <kapy@outlook.hu>
2020-03-12 15:04:42 +0100

    A szoba osztály repó és kontroller elkészült.

---
+++ src/main/java/hu/elte/Szobaeloszto/Entities/Szoba.java
@@ -0,0 +1,44 @@
+package hu.elte.Szobaeloszto.Entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Szoba implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer iD;
+
+    @Column
+    @NotNull
+    private String epuletNev;
+
+    @Column
+    @NotNull
+    private Integer emeletSzama;
+    
+    @Column
+    @NotNull
+    private Integer szobaSzama;
+    
+    @Column
+    @NotNull
+    private Integer feroHely;
+    
+}
