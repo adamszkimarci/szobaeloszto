ec694654d17d570a93695bf0dc675143599139c0
kapy <kapy@outlook.hu>
2020-05-16 11:53:04 +0200

    Fixek az előző hibához

--- src/test/java/hu/elte/Szobaeloszto/ControllerTest.java
+++ src/test/java/hu/elte/Szobaeloszto/ControllerTest.java
@@ -8,18 +8,18 @@ import hu.elte.Szobaeloszto.Repositories.SzobaRepository;
 import hu.elte.Szobaeloszto.Repositories.UserRepository;
 import hu.elte.Szobaeloszto.Security.AuthenticatedUser;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.security.test.context.support.WithMockUser;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@ExtendWith(SpringExtension.class)
-@WebMvcTest()
+@SpringBootTest
+@AutoConfigureMockMvc
 public class ControllerTest {
 @Autowired
 private MockMvc mockMvc;
@@ -48,22 +48,25 @@ UserRepository userRepo;
 @MockBean
 private BCryptPasswordEncoder passwordEncoder;
 
-
+@WithMockUser(value = "MVCtestprofile")
 @Test
 void whenValidInput_thenReturns200() throws Exception {
     mockMvc.perform(get("/diakok").contentType("application/json")).andExpect(status().is(200));
 }
 
+@WithMockUser(value = "MVCtestprofile")
 @Test
 void usersTest() throws Exception {
     mockMvc.perform(get("/users").contentType("application/json")).andExpect(status().is(404));
 }
 
+@WithMockUser(value = "MVCtestprofile")
 @Test
 void diakokTest() throws Exception {
     mockMvc.perform(get("/diakok").contentType("application/json")).andExpect(status().is(200));
 }
 
+@WithMockUser(value = "MVCtestprofile")
 @Test
 void szobakTest() throws Exception {
     mockMvc.perform(get("/szobak").contentType("application/json")).andExpect(status().is(200));
